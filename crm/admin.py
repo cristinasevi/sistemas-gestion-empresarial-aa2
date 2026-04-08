ac90fa0333118e65ac3e017a5da37e6e6488f27f
Cristina <133981266+cristinasevi@users.noreply.github.com>
2026-04-08 17:45:11 +0200

    Update panel admin

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Oportunidad
 
-# Register your models here.
+@admin.register(Oportunidad)
+class OportunidadAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'cliente', 'etapa', 'valor_estimado', 'fecha_cierre')
+    list_filter = ('etapa',)
+    search_fields = ('titulo', 'cliente__nombre')
